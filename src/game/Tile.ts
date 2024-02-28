68a8d7048c4e1a2852c9756a210e6ace1543bdd6
Tal Cohen <cohental63@gmail.com>
2024-02-28 08:48:40 +0200

    add Tile class

---
+++ src/game/Tile.ts
@@ -0,0 +1,9 @@
+export default class Tile {
+  constructor(
+    public image: HTMLImageElement,
+    public width: number,
+    public height: number,
+    public x: number,
+    public y: number
+  ) {}
+}
