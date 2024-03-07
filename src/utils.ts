08308651b756bdc7c18ace97062128b6ef1fe711
Tal Cohen <cohental63@gmail.com>
2024-03-07 18:16:55 +0200

    refactor utils functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,3 @@
-import { Tile } from "./d";
-import Doodler from "./game/doodler";
-
 //clamps a number between a min and max value
 export function clamp(num: number, min: number, max: number): number {
   return Math.min(Math.max(num, min), max);
@@ -9,16 +6,4 @@ export function clamp(num: number, min: number, max: number): number {
 // returns a random integer between min (inclusive) and max (exclusive)
 export function randInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min) + min);
-}
-
-//detects collision between doodler and platform
-export function detectCollision(doodle: Doodler, tile: Tile): boolean {
-  return (
-    doodle.dy > 0 &&
-    doodle.prevY + doodle.getHeight() < tile.y &&
-    doodle.y + doodle.getHeight() > tile.y &&
-    doodle.y < tile.y + tile.height &&
-    doodle.x + doodle.getWidth() > tile.x &&
-    doodle.x < tile.x + tile.width
-  );
 }
